23fd9298c566900774df03a23cda5b95688c64f0
lux-ed <lux-ed@users.noreply.github.com>
2018-11-30 14:03:58 +0100

    working file

---
+++ web_blocker.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp="hosts"
+hosts_path=r'/etc/hosts'
+redirect = '127.0.0.1'
+website_list=['www.facebook.com', 'facebook.com']
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 15) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 18):
+        print("Working hours...")
+        with open(hosts_path, "r+") as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+    else:
+        with open(hosts_path, 'r+') as file:   #r+ read and append
+            content=file.readlines()
+            file.seek(0)                       #put the pointer to the beginning
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("fun hours")
+    time.sleep(5)
